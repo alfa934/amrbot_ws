a2f2a9c2a0cd21302280fecef6760f03c7ba1752
alfa934 <alfarras934@gmail.com>
2026-09-06 15:25:03 +0700

    first commit

---
+++ src/amrbot_description/launch/display.launch.py
@@ -0,0 +1,46 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    pkg_share_dir = get_package_share_directory('amrbot_description')
+    
+    default_model_path = os.path.join(pkg_share_dir, 'urdf', 'amrbot_description.urdf.xacro')
+    default_rviz_path = os.path.join(pkg_share_dir, 'rviz', 'config.rviz')
+
+
+    declare_model = DeclareLaunchArgument(
+        name='model', default_value=default_model_path, 
+        description='Absolute path to robot model file'
+    )
+
+    declare_rviz_config = DeclareLaunchArgument(
+        name='rvizconfig', default_value=default_rviz_path, 
+        description='Absolute path to rviz config file'
+    )
+
+    robot_description = Command(['xacro ', LaunchConfiguration('model'), ' sim:=false'])
+
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', LaunchConfiguration('rvizconfig')]
+    )
+
+    return LaunchDescription([
+        declare_model,
+        declare_rviz_config,
+        robot_state_publisher_node,
+        rviz_node,
+    ])
