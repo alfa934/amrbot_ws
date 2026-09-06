102fb93d9e6233a8edafd3d9023c95773113283a
alfa934 <alfarras934@gmail.com>
2026-09-06 16:18:12 +0700

    modify display.launch.py

--- src/amrbot_description/launch/display.launch.py
+++ src/amrbot_description/launch/display.launch.py
@@ -7,10 +7,10 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    pkg_share_dir = get_package_share_directory('amrbot_description')
+    amrbot_description_pkg = get_package_share_directory('amrbot_description')
     
-    default_model_path = os.path.join(pkg_share_dir, 'urdf', 'amrbot_description.urdf.xacro')
-    default_rviz_path = os.path.join(pkg_share_dir, 'rviz', 'config.rviz')
+    default_model_path = os.path.join(amrbot_description_pkg, 'urdf', 'amrbot_description.urdf.xacro')
+    default_rviz_path = os.path.join(amrbot_description_pkg, 'rviz', 'config.rviz')
 
 
     declare_model = DeclareLaunchArgument(
